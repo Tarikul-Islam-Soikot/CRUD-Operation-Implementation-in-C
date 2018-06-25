850eedd1571b85a77b0f7f76cbff1aa1f5acf696
Tarikul <tarikulsoikot@gmail.com>
2018-06-25 12:35:47 +0600

    CRUD
    Create. Read, Update, Delete

---
+++ testConnection/testConnection/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace testConnection
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=desktop-75egoef\sqlexpress;Initial Catalog=Soikot;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            String query = "insert into Student(id, name, cgpa, balance, home_dist) values('" + textBox1.Text + "', '" + textBox2.Text + "', '" + textBox3.Text + "', '" + textBox4.Text + "', '" + textBox5.Text + "')";
+            SqlDataAdapter SDA = new SqlDataAdapter(query, con);
+            SDA.SelectCommand.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("successful insertion");
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            String query = "select * from Student";
+            SqlDataAdapter SDA = new SqlDataAdapter(query, con);
+            DataTable dt = new DataTable(); 
+            SDA.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+            MessageBox.Show("successful");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            String query = "update Student set name = '" + textBox2.Text + "', cgpa = '" + textBox3.Text + "', balance = '" + textBox4.Text + "', home_dist = '" + textBox5.Text + "' where id = '" + textBox1.Text + "' ";
+            SqlDataAdapter SDA = new SqlDataAdapter(query, con);
+            DataTable dt = new DataTable();
+            SDA.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+
+        }
+    }
+}
